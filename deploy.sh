c21965c0fe72214610c856a3f0d4a1d1db270ef6
haoyang.wu <haoyang.wu@thoughtworks.com>
2023-03-31 09:28:59 +0800

    feat: use azure function app

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,4 @@ APP_PLAN="${APP_PLAN:-your_default_app_plan}"
 
 az group create --name $RESOURCE_GROUP --location $LOCATION
 az appservice plan create --name $APP_PLAN --resource-group $RESOURCE_GROUP --sku B1 --is-linux
-az webapp create --resource-group $RESOURCE_GROUP --plan $APP_PLAN --name $APP_NAME --runtime "node|14-lts"
-az webapp config set --resource-group $RESOURCE_GROUP --name $APP_NAME --always-on true
-az webapp deployment source config --name $APP_NAME --resource-group $RESOURCE_GROUP --repo-url your_repo_url --branch master
+az functionapp create --resource-group $RESOURCE_GROUP --plan $APP_PLAN --name $APP_NAME --runtime "node|18-lts" --functions-version 4
