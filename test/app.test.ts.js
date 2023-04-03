5e8abbf604886a51c31d47ff58da80bf18b32861
why <18149106608@163.com>
2023-04-03 22:34:55 +0800

    chore: update the test

---
+++ test/app.test.ts.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import { isMedicalContent, containsSensitiveWords } from '../src/app';
+
+describe('isMedicalContent', () => {
+  it('should return true for medical content', () => {
+    const text = '心脏病是一种常见的疾病。';
+    expect(isMedicalContent(text)).to.be.true;
+  });
+
+  it('should return false for non-medical content', () => {
+    const text = '我喜欢吃苹果。';
+    expect(isMedicalContent(text)).to.be.false;
+  });
+});
+
+describe('containsSensitiveWords', () => {
+  it('should return true for text containing sensitive words', () => {
+    const text = '这是一个包含敏感词的句子。';
+    expect(containsSensitiveWords(text)).to.be.true;
+  });
+
+  it('should return false for text without sensitive words', () => {
+    const text = '这是一个不包含敏感词的句子。';
+    expect(containsSensitiveWords(text)).to.be.false;
+  });
+});
