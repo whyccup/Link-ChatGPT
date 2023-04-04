e0d5c85953069180b50749e453e981c6fd66abed
why <18149106608@163.com>
2023-04-04 22:54:54 +0800

    test: update gpt-client.test.ts

--- test/gpt-client.test.ts
+++ test/gpt-client.test.ts
@@ -1,14 +1,47 @@
-import { generateText, getModeList } from '../src/gpt-client';
-describe('generateText', () => {
-  it.skip('should generate text from prompt', async () => {
-    const prompt = 'Some prompt';
-    const result = await generateText(prompt);
-    console.log(result);
-    // expect(result.choices).toHaveLength(1);
-    // expect(typeof result.choices[0].text).toBe('string');
-  }, 100000); // 设置超时时间为10000毫秒（即10秒）
+import { Configuration, OpenAIApi } from 'openai';
+import { GPT_API_KEY, generateText, getModeList } from '../src/gpt-client';
 
-  it.skip('should return getModeList', async () => {
-    await getModeList();
+jest.mock('openai');
+
+describe('GPT Client', () => {
+  let openaiInstance: OpenAIApi;
+  let createCompletionSpy: jest.SpyInstance;
+  let listModelsSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    (OpenAIApi as jest.Mock).mockClear();
+    openaiInstance = new OpenAIApi();
+    createCompletionSpy = jest.spyOn(openaiInstance, 'createCompletion');
+    listModelsSpy = jest.spyOn(openaiInstance, 'listModels');
+  });
+
+  it('initializes OpenAIApi with the correct configuration', () => {
+    expect(Configuration).toHaveBeenCalledWith({
+      organization: 'org-lqKXheDHvoEcMIDUX4FbShAx',
+      apiKey: GPT_API_KEY
+    });
+    expect(OpenAIApi).toHaveBeenCalledTimes(1);
+  });
+
+  it.skip('generateText calls createCompletion with the correct parameters', async () => {
+    const mockCompletion = { data: 'test data' };
+    createCompletionSpy.mockResolvedValue(mockCompletion);
+
+    const result = await generateText('test prompt');
+    expect(createCompletionSpy).toHaveBeenCalledWith({
+      model: 'gpt-3.5-turbo',
+      prompt: 'test prompt',
+      max_tokens: 100
+    });
+    expect(result).toEqual(mockCompletion);
+  });
+
+  it.skip('getModeList calls listModels', async () => {
+    const mockModelList = { data: 'model list' };
+    listModelsSpy.mockResolvedValue(mockModelList);
+
+    const result = await getModeList();
+    expect(listModelsSpy).toHaveBeenCalled();
+    expect(result).toEqual(mockModelList);
   });
 });
