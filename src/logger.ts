4c75ea65ad05cfd55f02f1fe919fddea825a510e
why <18149106608@163.com>
2023-04-05 20:42:11 +0800

    feat: create logger.ts

---
+++ src/logger.ts
@@ -0,0 +1,19 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize(), winston.format.simple())
+    }),
+    new winston.transports.File({ filename: 'app.log' })
+  ]
+});
+
+export default logger;
