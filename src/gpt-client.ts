f3bc614dd399cd43504b4d370081541f29aae6d7
why <18149106608@163.com>
2023-04-10 21:13:01 +0800

    feat: update gpt-client.ts

--- src/gpt-client.ts
+++ src/gpt-client.ts
@@ -1,26 +1,33 @@
 import { Configuration, OpenAIApi } from 'openai';
 import dotenv from 'dotenv';
+
 dotenv.config();
 const GPT_API_KEY = process.env.GPT_API_KEY;
 
 const configuration = new Configuration({
-  organization: 'org-lqKXheDHvoEcMIDUX4FbShAx',
   apiKey: GPT_API_KEY
 });
 
 const openai = new OpenAIApi(configuration);
 
 async function generateText(prompt: string) {
-  const result = await openai.createCompletion({
+  return await openai.createCompletion({
     model: 'gpt-3.5-turbo',
     prompt: prompt,
     max_tokens: 100
   });
-  return result;
+}
+
+async function generateChat(content: string) {
+  return await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: content }],
+    max_tokens: 100
+  });
 }
 
 async function getModeList() {
   const response = await openai.listModels();
   return response;
 }
-export { GPT_API_KEY, openai, generateText, getModeList };
+export { GPT_API_KEY, openai, generateText, generateChat, getModeList };
